fa3a12d53f065d0dcd5ce38c10ab2e9a715bd817
Simon 42 <Monkeymandeveloper@protonmail.com>
2024-01-19 15:57:56 +0100

    bash like prompt for readline

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: svolodin <svolodin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 13:52:15 by albeninc          #+#    #+#             */
-/*   Updated: 2024/01/19 14:16:50 by svolodin         ###   ########.fr       */
+/*   Updated: 2024/01/19 15:12:15 by svolodin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 # define MINISHELL_H
 
 # include "libft.h"
+# include <readline/history.h>
+# include <readline/readline.h>
+
+void	show_hist(void);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,14 +6,55 @@
 /*   By: svolodin <svolodin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 14:06:41 by svolodin          #+#    #+#             */
-/*   Updated: 2024/01/19 14:15:52 by svolodin         ###   ########.fr       */
+/*   Updated: 2024/01/19 15:50:25 by svolodin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+char	*get_prompt(void)
+{
+	char	*username;
+	char	*hostname;
+	char	cwd[1024];
+	char	*prompt;
+
+	prompt = malloc(1024 * sizeof(char));
+	if (prompt == NULL)
+		return (NULL);
+	prompt[0] = '\0';
+	username = getenv("USER");
+	if (username == NULL)
+		username = getenv("LOGNAME");
+	hostname = "minishell";
+	getcwd(cwd, sizeof(cwd));
+	if (username)
+		ft_strlcat(prompt, username, 1024);
+	ft_strlcat(prompt, "@", 1024);
+	ft_strlcat(prompt, hostname, 1024);
+	ft_strlcat(prompt, ":", 1024);
+	ft_strlcat(prompt, cwd, 1024);
+	ft_strlcat(prompt, "$ ", 1024);
+	return (prompt);
+}
+
 int	main(void)
 {
-	ft_printf("hello World\n");
-	return(0);
+	char	*input;
+	char	*prompt;
+
+	prompt = get_prompt();
+	while (42)
+	{
+		input = readline(prompt);
+		if (input == NULL)
+			break ;
+		if (*input)
+			add_history(input);
+		if (strcmp(input, "history") == 0)
+			show_hist();
+		free(input);
+	}
+	free(prompt);
+	return (0);
 }
