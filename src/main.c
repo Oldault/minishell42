e69c608fa59ef59ac5494bbaf24d9c4d584f088e
Simon 42 <Monkeymandeveloper@protonmail.com>
2024-01-19 17:12:12 +0100

    exit and env cmd added

--- src/main.c
+++ src/main.c
@@ -6,17 +6,41 @@
 /*   By: svolodin <svolodin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 14:06:41 by svolodin          #+#    #+#             */
-/*   Updated: 2024/01/19 16:16:36 by svolodin         ###   ########.fr       */
+/*   Updated: 2024/01/19 17:09:22 by svolodin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+void execute_env(char **env)
+{
+	int	i;
+
+	i = -1;
+    while (env[++i] != NULL)
+        printf("%s\n", env[i]);
+}
+
+void	handle_input(char *input, char **env)
+{
+	if (ft_strncmp(input, "history", 8) == 0)
+			show_hist();
+	else if (ft_strncmp(input, "exit", 5) == 0)
+	{
+		free(input);
+		exit(0);
+	}
+	else if (ft_strncmp(input, "env", 4) == 0)
+		execute_env(env);
+}
+
+int	main(int ac, char **av, char ** env)
 {
 	char	*input;
 	char	*prompt;
 
+	(void)ac;
+	(void)av;
 	prompt = get_prompt();
 	while (42)
 	{
@@ -25,8 +49,7 @@ int	main(void)
 			break ;
 		if (*input)
 			add_history(input);
-		if (strcmp(input, "history") == 0)
-			show_hist();
+		handle_input(input, env);
 		free(input);
 	}
 	free(prompt);
