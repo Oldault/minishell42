dd26c657341685694d49761032af157c1c46b99a
Alessio Benincasa <albeninc@paul-f3Br3s5.clusters.42paris.fr>
2024-01-22 20:11:49 +0100

    added prompt, fixed the bug

--- src/prompt.c
+++ src/prompt.c
@@ -12,62 +12,53 @@
 
 #include "minishell.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #define PROMPT_SIZE 1024
 #define PATH_SIZE 1024
 
-static void	shorten_dir(const char *home_dir, const char *cwd, char *rel_cwd)
-{
-	if (home_dir && ft_strncmp(cwd, home_dir, ft_strlen(home_dir)) == 0)
-	{
-		rel_cwd[0] = '~';
-		ft_strlcpy(rel_cwd + 1, cwd + ft_strlen(home_dir), PATH_SIZE - 1);
-	}
-	else
-		ft_strlcpy(rel_cwd, cwd, PATH_SIZE);
-}
-char	*part2_prompt(char *prompt)
-{
-	char	*home_dir;
-	char	cwd[PATH_SIZE];
-	char	rel_cwd[PATH_SIZE];
-
-	home_dir = getenv("HOME");
-	getcwd(cwd, sizeof(cwd));
-	shorten_dir(home_dir, cwd, rel_cwd);
-	ft_strlcat(prompt, COLOR_BLUE, PROMPT_SIZE);
-	ft_strlcat(prompt, rel_cwd, PROMPT_SIZE);
-	ft_strlcat(prompt, "$ ", PROMPT_SIZE);
-	ft_strlcat(prompt, COLOR_RESET, PROMPT_SIZE);
-	return (prompt);
+// Cette fonction raccourcit le chemin en remplaçant le répertoire personnel par '~'
+void shorten_path(char *path, const char *home_dir) {
+    if (home_dir && strncmp(path, home_dir, strlen(home_dir)) == 0) {
+        memmove(path, path + strlen(home_dir), strlen(path) - strlen(home_dir) + 1);
+        path[0] = '~';  // Remplacer le début par '~'
+    }
 }
 
-char	*part1_prompt(char *prompt)
-{
-	char	*username;
-	char	*hostname;
-	
-	username = getenv("USER");
-	if (username == NULL)
-		username = getenv("LOGNAME");
-	hostname = "minishell";
-	ft_strlcat(prompt, COLOR_GREEN, PROMPT_SIZE);
-	if (username)
-		ft_strlcat(prompt, username, PROMPT_SIZE);
-	ft_strlcat(prompt, "@", PROMPT_SIZE);
-	ft_strlcat(prompt, hostname, PROMPT_SIZE);
-	ft_strlcat(prompt, ":", PROMPT_SIZE);
-	return (prompt);
-}
+char *get_prompt(void) {
+    char *prompt = malloc(PROMPT_SIZE);
+    if (prompt == NULL) {
+        perror("Unable to allocate memory for prompt");
+        return NULL;
+    }
 
-char	*get_prompt(void)
-{
-	char	*prompt;
+    char cwd[PATH_SIZE];
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        perror("Unable to get current working directory");
+        free(prompt);
+        return NULL;
+    }
 
-	prompt = malloc(PROMPT_SIZE * sizeof(char));
-	if (prompt == NULL)
-		return (NULL);
-	prompt[0] = '\0';
-	prompt = part1_prompt(prompt);
-	prompt = part2_prompt(prompt);
-	return (prompt);
+    const char *home_dir = getenv("HOME");
+    const char *username = getenv("USER");
+    if (username == NULL) {
+        username = "user";  // Nom d'utilisateur par défaut si USER n'est pas défini
+    }
+
+    shorten_path(cwd, home_dir);
+
+    // Réinitialiser le contenu de prompt
+    prompt[0] = '\0';
+
+    // Construction du prompt
+    strncat(prompt, "[", PROMPT_SIZE - strlen(prompt) - 1);
+    strncat(prompt, username, PROMPT_SIZE - strlen(prompt) - 1);
+    strncat(prompt, "@minishell ", PROMPT_SIZE - strlen(prompt) - 1);
+    strncat(prompt, cwd, PROMPT_SIZE - strlen(prompt) - 1);
+    strncat(prompt, "]$ ", PROMPT_SIZE - strlen(prompt) - 1);
+
+    return prompt;
 }
