b6a6e6e105c39864e0632af4b5ca287893b487a7
Alessio Benincasa <albeninc@paul-f3Br8s3.clusters.42paris.fr>
2024-01-23 15:22:58 +0100

    done

--- src/prompt.c
+++ src/prompt.c
@@ -6,7 +6,7 @@
 /*   By: svolodin <svolodin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 16:16:23 by svolodin          #+#    #+#             */
-/*   Updated: 2024/01/19 17:29:57 by svolodin         ###   ########.fr       */
+/*   Updated: 2024/01/23 15:11:33 by albeninc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,25 @@
 #define PROMPT_SIZE 1024
 #define PATH_SIZE 1024
 
-// Cette fonction raccourcit le chemin en remplaçant le répertoire personnel par '~'
 void shorten_path(char *path, const char *home_dir) {
     if (home_dir && strncmp(path, home_dir, strlen(home_dir)) == 0) {
-        memmove(path, path + strlen(home_dir), strlen(path) - strlen(home_dir) + 1);
-        path[0] = '~';  // Remplacer le début par '~'
+        size_t home_dir_length = strlen(home_dir);
+        size_t path_length = strlen(path);
+
+        // If the path is exactly the home directory or the home directory plus a slash, replace with "~"
+        if (home_dir_length == path_length || (home_dir_length + 1 == path_length && path[home_dir_length] == '/')) {
+            strcpy(path, "~");
+        } else {
+            // Replace the home directory part with "~/" and shift the rest of the path
+            // The +1 in the memmove start position skips the slash after the home directory
+            memmove(path + 2, path + home_dir_length + 1, path_length - home_dir_length);
+            path[0] = '~';
+            path[1] = '/';
+            path[path_length - home_dir_length + 1] = '\0'; // Properly terminate the string
+        }
     }
 }
+
 
 char *get_prompt(void) {
     char *prompt = malloc(PROMPT_SIZE);
