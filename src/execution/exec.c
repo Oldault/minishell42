032df6bccc665dd3514fe7b2a697f791bc0e9d17
Alessio Benincasa <albeninc@paul-f5Ar3s1.clusters.42paris.fr>
2024-02-14 11:41:18 +0100

    done the cat and CTRL + \ handling

--- src/execution/exec.c
+++ src/execution/exec.c
@@ -30,16 +30,18 @@ void	reset_file_descriptors(t_mini *data, int i)
 
 void	handle_wait(t_exec_cmd *exec_data, int i)
 {
-	int status;
-	waitpid(exec_data->child_pids[i], &status, 0);
+	int	status;
+	int	termsig;
 
+	waitpid(exec_data->child_pids[i], &status, 0);
 	if (WIFEXITED(status))
 	{
 		last_exit_status = WEXITSTATUS(status);
+			// Set the last exit status based on the child's exit status
 	}
 	else if (WIFSIGNALED(status))
 	{
-		int termsig = WTERMSIG(status);
+		termsig = WTERMSIG(status);
 		if (termsig == SIGQUIT)
 		{
 			printf("Quit (core dumped)\n");
@@ -47,59 +49,57 @@ void	handle_wait(t_exec_cmd *exec_data, int i)
 		last_exit_status = 128 + termsig;
 	}
 	else
-	{
 		last_exit_status = 127;
-	}
+}
 
-	int exec_cmd_seg(t_mini * data, t_exec_cmd * exec_data, int i)
+int	exec_cmd_seg(t_mini *data, t_exec_cmd *exec_data, int i)
+{
+	exec_data->cmd_path = find_path(data->paths, data->cmds[i]);
+	if (!handle_cmd_path(data->cmds[i][0], exec_data->cmd_path, exec_data))
+		return (0);
+	setup_pipes(exec_data->pipe_fds, i, exec_data->num_cmds);
+	apply_redirections(data, i);
+	if (data->err != NULL)
+		printf("%s\n", data->err);
+	else
+		execute_single_command(data, exec_data, i);
+	free(exec_data->cmd_path);
+	if (exec_data->pipe_end != -1)
+		close(exec_data->pipe_end);
+	if (i < exec_data->num_cmds - 1)
 	{
-		exec_data->cmd_path = find_path(data->paths, data->cmds[i]);
-		if (!handle_cmd_path(data->cmds[i][0], exec_data->cmd_path, exec_data))
-			return (0);
-		setup_pipes(exec_data->pipe_fds, i, exec_data->num_cmds);
-		apply_redirections(data, i);
-		if (data->err != NULL)
-			printf("%s\n", data->err);
-		else
-			execute_single_command(data, exec_data, i);
-		free(exec_data->cmd_path);
-		if (exec_data->pipe_end != -1)
-			close(exec_data->pipe_end);
-		if (i < exec_data->num_cmds - 1)
-		{
-			exec_data->pipe_end = exec_data->pipe_fds[0];
-			close(exec_data->pipe_fds[1]);
-		}
-		else
-			exec_data->pipe_end = -1;
-		reset_file_descriptors(data, i);
-		return (1);
+		exec_data->pipe_end = exec_data->pipe_fds[0];
+		close(exec_data->pipe_fds[1]);
 	}
+	else
+		exec_data->pipe_end = -1;
+	reset_file_descriptors(data, i);
+	return (1);
+}
 
-	void execute_commands(t_mini * data)
+void	execute_commands(t_mini *data)
+{
+	t_exec_cmd	*exec_data;
+	int			i;
+
+	if (data->cmds == NULL || data->cmds[0] == NULL || data->cmds[0][0] == NULL)
+		return ;
+	exec_data = init_exec_data(data);
+	if (exec_data->num_cmds == 1 && handle_builtin(data))
 	{
-		t_exec_cmd *exec_data;
-		int i;
-
-		if (data->cmds == NULL || data->cmds[0] == NULL
-			|| data->cmds[0][0] == NULL)
-			return ;
-		exec_data = init_exec_data(data);
-		if (exec_data->num_cmds == 1 && handle_builtin(data))
-		{
-			free(exec_data->child_pids);
-			free(exec_data);
-			return ;
-		}
-		i = -1;
-		while (++i < exec_data->num_cmds)
-		{
-			if (!exec_cmd_seg(data, exec_data, i))
-				return ;
-		}
-		i = -1;
-		while (++i < exec_data->num_cmds)
-			handle_wait(exec_data, i);
 		free(exec_data->child_pids);
 		free(exec_data);
+		return ;
 	}
+	i = -1;
+	while (++i < exec_data->num_cmds)
+	{
+		if (!exec_cmd_seg(data, exec_data, i))
+			return ;
+	}
+	i = -1;
+	while (++i < exec_data->num_cmds)
+		handle_wait(exec_data, i);
+	free(exec_data->child_pids);
+	free(exec_data);
+}
